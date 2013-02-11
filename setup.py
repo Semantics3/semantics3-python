3f66ef819c3c0abd246f166908833872088d707c
Shawn Tan <shawn@wtf.sg>
2013-02-10 18:16:08 -0800

    Included setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install_requires = [
+	'oauth2 >= 1.5.211',
+	]
+
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+setup(
+		name = "semantics3",
+		version = "0.0.1",
+		author = "Shawn Tan",
+		author_email = "shawn@semantics3.com",
+		description = ("Semantics3 Products API"),
+		license = "MIT",
+		keywords = "api ecommerce products",
+		url = "https://github.com/Semantics3/semantics3-python",
+		packages=['semantics3'],
+		long_description=read('README.md'),
+		install_requires = install_requires,
+		classifiers = [
+			"Development Status :: 3 - Alpha",
+			"Topic :: Utilities",
+			"License :: OSI Approved :: MIT License",
+			]
+		)
